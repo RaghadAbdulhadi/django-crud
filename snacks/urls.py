e47193a6e9a317077746143dd1f44bfba01273a0
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-05-19 12:58:59 +0300

    Add CRUD links

--- snacks/urls.py
+++ snacks/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path('<int:pk>', SnackDetailView.as_view(), name='detail_snack'),
     path('update/<int:pk>', SnackUpdateView.as_view(), name='update_snack'),
     path('delete/<int:pk>', SnackDeleteView.as_view(), name='delete_snack'),
-
 ]
